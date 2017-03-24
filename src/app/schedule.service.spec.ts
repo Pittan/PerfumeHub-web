e2e89c1329a33193a842813659e53970ed42268c
Amon Keishima <amon.keishima@gmail.com>
2017-03-25 00:43:50 +0900

    ScheduleServiceの追加

---
+++ src/app/schedule.service.spec.ts
@@ -0,0 +1,39 @@
+import { TestBed, async, inject } from '@angular/core/testing';
+
+import { ScheduleService } from './schedule.service';
+
+import { HttpModule, BaseRequestOptions, Http, Response, ResponseOptions } from '@angular/http';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+import { Schedule } from './schedule';
+
+describe('ScheduleService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [ScheduleService,
+        {
+          provide: Http,
+          useFactory: (backend, options) => new Http(backend, options),
+          deps: [MockBackend, BaseRequestOptions]
+        },
+        MockBackend, BaseRequestOptions]
+    });
+  });
+
+  it('should ...', inject([ScheduleService], (service: ScheduleService) => {
+    expect(service).toBeTruthy();
+  }));
+
+  it('userInfo', async(inject([ScheduleService, MockBackend], (schedule: ScheduleService, backend: MockBackend) => {
+
+    backend.connections.subscribe((conn: MockConnection) => {
+      let ops = new ResponseOptions({body: {'schedule' : [{body: 'hogehoge'},{body: 'foobar'}]} });
+      conn.mockRespond(new Response(ops));
+    });
+
+    schedule.fetchSchedule().subscribe((schedules: Schedule[]) => {
+      expect(schedules).not.toBeNull();
+      expect(schedules[0].body).toBe('hogehoge');
+    });
+
+  })));
+});

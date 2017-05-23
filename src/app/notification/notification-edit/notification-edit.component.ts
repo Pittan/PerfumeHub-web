e0558635b0b1bfc48ac07d133eb11945df6896ed
Amon Keishima <amon.keishima@gmail.com>
2017-05-24 00:05:37 +0900

    ✨ 即時で通知を送信できるようにした
    ownerかmaster権限を持っている人がnotification/newにアクセスすると通知を送信できるようになっています。
    必要な項目を埋めて送信ボタンを押すと送信されます。
    
    *連打しただけ送れてしまう
    *送信したログが残らない
    *何も入れなくても送信した挙動になる（送信はできていない）
    
    追って修正はしますが、とりあえず通知を送りたいのでこのままアップします。

--- src/app/notification/notification-edit/notification-edit.component.ts
+++ src/app/notification/notification-edit/notification-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiBaseService } from '../../core/api-base.service';
 
 @Component({
   selector: 'ph-notification-edit',
@@ -7,17 +8,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotificationEditComponent implements OnInit {
 
-  public notification_type: string;
+  public notificationType = 'normal';
+  public channel = 'all';
   public header: string;
   public body: string;
+  public image_url: string;
   public website_url: string;
+  public transition = 'PHNewsTransition';
 
-  constructor() { }
+  constructor(private apiBase: ApiBaseService) { }
 
   ngOnInit() {
+
   }
 
   submit() {
-    alert('通知を送る予定');
+    const param = {
+      type: this.notificationType,
+      title: this.header,
+      body: this.body,
+      image_url: this.image_url,
+      channel: null,
+      url: null,
+      transition: null
+    };
+
+    if (this.channel !== 'all') {
+      param.channel = this.channel;
+    }
+
+    if (this.notificationType === 'normal') {
+      param.url = this.website_url;
+    }
+
+    if (this.notificationType === 'pta') {
+      param.transition = this.transition;
+    }
+
+    if (confirm('送信します。よろしいですか？')) {
+      this.apiBase.post('notification/', param)
+        .map( res => res.json() )
+        .subscribe(
+          res => {
+            if (res.error_message) {
+              alert(res.error_message + '(' + res.error_code + ')');
+            } else {
+              alert(res.message);
+            }
+          }
+        );
+    }
+
   }
 }

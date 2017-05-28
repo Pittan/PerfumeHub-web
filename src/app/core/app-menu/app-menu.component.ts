8023b2968e3c513f5e0e87a1169b175765631315
Amon Keishima <amon.keishima@gmail.com>
2017-05-28 21:46:54 +0900

    ✨ メニューにアニメーションを追加

--- src/app/core/app-menu/app-menu.component.ts
+++ src/app/core/app-menu/app-menu.component.ts
@@ -4,11 +4,34 @@ import { Subscription } from 'rxjs/Subscription';
 import { environment } from '../../../environments/environment';
 import { UserService } from '../user.service';
 import { User } from '../user';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'ph-app-menu',
   templateUrl: './app-menu.component.html',
-  styleUrls: ['./app-menu.component.styl']
+  styleUrls: ['./app-menu.component.styl'],
+  animations: [
+    trigger('menuState', [
+      state('inactive', style({
+        transform: 'translateY(-100%)'
+      })),
+      state('active',   style({
+        transform: 'translateY(0%)'
+      })),
+      transition('inactive => active', animate('200ms ease-in-out')),
+      transition('active => inactive', animate('200ms ease-in-out'))
+    ]),
+    trigger('bgState', [
+      state('inactive', style({
+        backgroundColor: 'transparent'
+      })),
+      state('active',   style({
+        backgroundColor: 'rgba(0, 0, 0, .7)'
+      })),
+      transition('inactive => active', animate('200ms ease-in-out')),
+      transition('active => inactive', animate('200ms ease-in-out'))
+    ]),
+  ]
 })
 export class AppMenuComponent implements OnInit, OnDestroy {
 
@@ -16,6 +39,7 @@ export class AppMenuComponent implements OnInit, OnDestroy {
 
   public appVersion: string;
   public user: User = null;
+  public menuState: string;
 
   private subscription = new Subscription();
 
@@ -23,8 +47,13 @@ export class AppMenuComponent implements OnInit, OnDestroy {
               private userService: UserService) { }
 
   ngOnInit() {
+    this.menuState = 'inactive';
+
     const headerStatusSub = this.headerService.isMenuOpen.subscribe(isOpen => {
-      this.isMenuOpen = isOpen ? 'block' : 'none';
+      if (isOpen) {
+        this.isMenuOpen = 'block';
+      }
+      this.menuState = isOpen ? 'active' : 'inactive';
     });
     this.subscription.add(headerStatusSub);
 
@@ -47,6 +76,14 @@ export class AppMenuComponent implements OnInit, OnDestroy {
 
   logout() {
     this.userService.logout(window.location.href);
+  }
+
+  dismiss() {
+    this.headerService.setIsMenuOpen(false);
+  }
+
+  onAnimeDone() {
+    this.isMenuOpen = this.menuState === 'active' ? 'block' : 'none';
   }
 
 }

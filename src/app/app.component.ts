a91f8f7c2b0dcf865c3a66d345fbb60f092ae66c
Amon Keishima <amon.keishima@gmail.com>
2017-06-04 01:49:21 +0900

    ✨ ネイティブ連携の方法を変更

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,8 @@ import { HeaderService } from './core/header/header.service';
 
 declare global {
   interface Window {
-    perfumeHub: any;
+    WebViewJavascriptBridge: any;
+    WVJBCallbacks: any;
   }
 }
 
@@ -16,22 +17,37 @@ declare global {
 })
 export class AppComponent implements OnInit {
 
-  constructor(private headerService: HeaderService, angulartics2GoogleAnalytics: Angulartics2GoogleAnalytics,
-  private appRef: ApplicationRef) {}
+  constructor(private headerService: HeaderService,
+              angulartics2GoogleAnalytics: Angulartics2GoogleAnalytics,
+              private appRef: ApplicationRef) {}
+
+  setupWebViewJavascriptBridge(callback) {
+    if (Window.prototype.WebViewJavascriptBridge) { return callback(Window.prototype.WebViewJavascriptBridge); }
+    if (Window.prototype.WVJBCallbacks) { return window.WVJBCallbacks.push(callback); }
+    window.WVJBCallbacks = [callback];
+    const WVJBIframe = document.createElement('iframe');
+    WVJBIframe.style.display = 'none';
+    WVJBIframe.src = 'https://__bridge_loaded__';
+    document.documentElement.appendChild(WVJBIframe);
+    setTimeout(function() { document.documentElement.removeChild(WVJBIframe); }, 0);
+  }
 
   ngOnInit() {
 
-    Window.prototype.perfumeHub = {};
+    this.setupWebViewJavascriptBridge(bridge => {
 
-    /**
-     * テーマカラーを設定する
-     * @param color
-     */
-    Window.prototype.perfumeHub.setThemeColor = (color: string) => {
-      this.headerService.setColor(color);
-      // tickしてビューに反映させる
-      this.appRef.tick();
-    };
+      // 色の変更を行う
+      bridge.registerHandler('setThemeColor', (data, responseCallback) => {
+        this.headerService.setColor(data);
+        // tickしてビューに反映させる
+        this.appRef.tick();
+        responseCallback(data);
+      });
+
+      // ネイティブにアプリの準備ができたことを伝える
+      bridge.callHandler('App Ready', {}, responseData => { });
+
+    });
   }
 
 }
